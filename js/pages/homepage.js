d747467cea2994ca10f510cd5f43d61032a5919b
Mr.Jia <13253577806@163.com@48da1774-f64d-42cd-90ea-e7d6bc6823cd>
2018-07-02 13:16:54 +0000

    no commit message

--- js/pages/homepage.js
+++ js/pages/homepage.js
@@ -8,9 +8,10 @@ $(function(){
 		$(this).click(function(){
 			$(this).addClass("active");
 			$(this).siblings().removeClass("active");
-			var code = $(this).attr("code");
-			console.log(code)
-			$(".content iframe").attr("src", code + ".html")
+//			var code = $(this).attr("code");
+//			console.log(code)
+//			$(".content iframe").attr("src", code + ".html");
+//			$(".content iframe").attr("target",code + ".html");
 			
 		})
 	})
